b6fb8995c2c91c9d5100b56042a6a53cbf926880
Devrim Caner ÖKDEM <DevrimCanerOkdem@outlook.com>
2023-11-25 19:18:01 +0300

    grade add

--- student-tracking-system/library/addGrade.php
+++ student-tracking-system/library/addGrade.php
@@ -11,14 +11,59 @@ if(!$_POST){
 }
 
 //Get Data
-$name = isset($_POST['name']) ? $_POST['name'] : null;
-$name = isset($_POST['name']) ? $_POST['name'] : null;
-$name = isset($_POST['name']) ? $_POST['name'] : null;
-$name = isset($_POST['name']) ? $_POST['name'] : null;
+$student = isset($_POST['student']) ? $_POST['student'] : null;
+$course = isset($_POST['course']) ? $_POST['course'] : null;
+$type = isset($_POST['type']) ? $_POST['type'] : null;
+$grade = isset($_POST['grade']) ? $_POST['grade'] : null;
 
 //Controls
-if(!$name){
-    $response['message'] = 'Ders İsimi bölümü boş olamaz';
+if(!$student){
+    $response['message'] = 'Öğrenci verisi boş olamaz';
+    return Response($response);
+}
+if(!$course){
+    $response['message'] = 'Ders verisi boş olamaz';
+    return Response($response);
+}
+if(!$type){
+    $response['message'] = 'Tür verisi boş olamaz';
+    return Response($response);
+}
+if(!$grade){
+    $response['message'] = 'Not verisi boş olamaz';
+    return Response($response);
+}
+
+if(!is_int($student)){
+    $response['message'] = 'Hatalı Öğrenci verisi.';
+    return Response($response);
+}
+if(!is_int($course)){
+    $response['message'] = 'Hatalı Ders verisi';
+    return Response($response);
+}
+if(!is_int($type)){
+    $response['message'] = 'Hatalı Tür verisi';
+    return Response($response);
+}
+if(!is_int($grade)){
+    $response['message'] = 'Hatalı Not verisi';
+    return Response($response);
+}
+if($grade < 0 && $grade > 100){
+    $response['message'] = 'Not 0 ile 100 arasında olmalıdır.';
+    return Response($response);
+}
+//Does student exist ?
+$checkStudent = $db->query("SELECT * FROM students WHERE id = '{$student}'")->fetch(PDO::FETCH_ASSOC);
+if(!$checkStudent){
+    $response['message'] = 'Öğrenci Bulunamadı.';
+    return Response($response);
+}
+//Does course exist ?
+$checkCourse = $db->query("SELECT * FROM courses WHERE id = '{$course}'")->fetch(PDO::FETCH_ASSOC);
+if(!$checkCourse){
+    $response['message'] = 'Ders Bulunamadı.';
     return Response($response);
 }
 
