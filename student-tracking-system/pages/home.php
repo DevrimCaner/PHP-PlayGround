7d52e9e7f9e157e574e1861e08cc6f840fb76572
Devrim Caner ÖKDEM <DevrimCanerOkdem@outlook.com>
2023-11-28 14:43:19 +0300

    Home Page

--- student-tracking-system/pages/home.php
+++ student-tracking-system/pages/home.php
@@ -1,5 +1,41 @@
 <section  class="pt-4">
     <div class="container">
-    <h1 class="text-primary fs-2">Ana Sayfa</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <img class="w-100 rounded-4 border border-primary shadow mb-4" src="./images/main.jpg" alt="">
+            </div>
+            <div class="col-md-6">
+                <h1 class="text-primary fs-2">Öğrenci Takip sistemi</h1>
+                <p class="mt-4">
+                Öğrenci Takip Sistemi, öğrenci ve ders yönetimini kolaylaştırmak üzere tasarlanmış kullanıcı dostu bir uygulamadır.
+                Basit ve şık arayüzü, verilerin güvenli bir şekilde saklanmasını ve kullanıcıların eğitim sürecini daha etkili bir şekilde yönetmelerini amaçlar.
+                </p>
+                <a href="index.php?page=students" class="btn btn-outline-primary border-primary rounded-pill shadow mt-5">Öğrencileri görüntüle</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section  class="pt-4">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <h2 class="text-primary"><span class="badge bg-primary bg-gradient rounded-3"><i class="bi bi-journal-text"></i></span> Not Takibi</h2>
+                <p class="mt-4">
+                Eğitim sürecini daha etkili ve düzenli bir şekilde yönetin! Bu kullanıcı dostu uygulama, öğrenci ve ders yönetimini kolaylaştırmak amacıyla tasarlandı. İşte uygulamamızın öne çıkan özellikleri:
+                </p>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h2 class="text-primary"><span class="badge bg-success bg-gradient rounded-3"><i class="bi bi-window"></i></span> Kullanıcı Dostu Arayüz</h2>
+                <p class="mt-4">
+                Basit ve şık tasarımı sayesinde uygulamayı kullanmak çok kolay. İhtiyacınıza uygun bir şekilde özelleştirilebilir arayüz, sizin için ideal deneyimi sunar.
+                </p>
+            </div>
+            <div class="col-md-4 mb-4">
+                <h2 class="text-primary"><span class="badge bg-purple bg-gradient rounded-3"><i class="bi bi-book"></i></span> Ders Yönetimi</h2>
+                <p class="mt-4">
+                Ders eklemek, çıkartmak ve güncellemek artık birkaç tıklama ile mümkün. Hangi dersin hangi öğrencilere açık olduğunu kontrol etmek hiç bu kadar basit olmamıştı.
+                </p>
+            </div>
+        </div>
     </div>
 </section>
